2514a451286863a246e2356db608574ac92b7c00
ChaseTheAce <32078056+ChaseTheAce@users.noreply.github.com>
2018-02-21 17:14:31 -0600

    Coin Collection

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public static int CoinCount { get; private set; }
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+
+    private void Start()
+    {
+        CoinCount = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            CoinCount++;
+            Debug.Log("Touched the Coin! coinCount is " + CoinCount);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+
+
+        }
+
+
+    }
+}
