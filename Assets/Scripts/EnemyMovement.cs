f1cd5ae287b441ca7b3de05ae4f707231f0bf88f
Scubafishy <32077848+Scubafishy@users.noreply.github.com>
2018-02-21 22:30:58 -0600

    Added Enemy Movment
    added movment to the enemies. Can control how fast they can move, and how far their range is

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed;
+
+    [SerializeField]
+    float OutOfRangeNumber;
+
+    [SerializeField]
+    Transform Player;
+
+    private float Range;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Range = Vector2.Distance(transform.position, Player.transform.position);
+        if (Range < OutOfRangeNumber)
+        {
+            float step = movementSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
+        }
+    }
+}
