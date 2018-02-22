85a8354133c39cf64a5fb69233484b089c619530
Scubafishy <32077848+Scubafishy@users.noreply.github.com>
2018-02-21 22:41:13 -0600

    Removed range from movment
    jumped the gun, didn't work

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,21 +8,16 @@ public class EnemyMovement : MonoBehaviour {
     float movementSpeed;
 
     [SerializeField]
-    float OutOfRangeNumber;
-
-    [SerializeField]
     Transform Player;
 
-    private float Range;
+ 
 
     // Update is called once per frame
     void Update ()
     {
-        Range = Vector2.Distance(transform.position, Player.transform.position);
-        if (Range < OutOfRangeNumber)
-        {
+       
             float step = movementSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
-        }
+       
     }
 }
